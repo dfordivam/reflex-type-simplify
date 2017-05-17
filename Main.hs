b52f8e9d64da778768e97f368b1f266e8f6357ad
Divam <dfordivam@gmail.com>
2017-05-17 11:19:00 +0900

    Add monadic support to parsing

--- Main.hs
+++ Main.hs
@@ -7,16 +7,11 @@ import Language.Haskell.Exts
 import Control.Monad
 
 data ReflexAst where
+  MonadicT :: ReflexAst -> ReflexAst
   DynamicT :: ReflexAst -> ReflexAst
   EventT :: ReflexAst -> ReflexAst
   MapT :: Show l => (Type l) -> ReflexAst -> ReflexAst
   TypeVar :: Show l => (Type l) -> ReflexAst
-
-instance Show ReflexAst where
-  show (DynamicT ast) = "Dynamic t (" ++ show ast ++ ")"
-  show (EventT ast) = "Event t (" ++ show ast ++ ")"
-  show (MapT k ast) = "Map (" ++ prettyPrint k ++ ") (" ++ show ast ++ ")"
-  show (TypeVar v) = prettyPrint v
 
 main = undefined
 
@@ -34,13 +29,15 @@ doParseType (TyParen _ t) = doParseType t
 doParseType v@SomeType = Right (TypeVar v)
 doParseType v@(TyVar _ n) = Right (TypeVar v)
 
-doParseType _ = Left "Error parsing type"
+doParseType _t = Left $ "Error parsing type:" ++ show _t
 
 -- applyReflexAst :: ReflexAst -> ReflexAst -> ReflexAst
 -- applyReflexAst (DynamicT )
 
 pattern SomeType <-
   (TyCon _ (UnQual _ (Ident _ _)))
+pattern Monadic <-
+  (TyVar _ (Ident _ "m"))
 pattern Dynamic <-
   (TyApp _ (TyCon _ (UnQual _ (Ident _ "Dynamic"))) (TyVar _ (Ident _ t)))
 pattern Event <-
@@ -49,7 +46,15 @@ pattern Map v <-
   (TyApp _ (TyCon _ (UnQual _ (Ident _ "Map"))) v)
 
 makeAst :: (Show l) => Type l -> Type l -> Either String ReflexAst
+makeAst Monadic t2 = MonadicT <$> doParseType t2
 makeAst Dynamic t2 = DynamicT <$> doParseType t2
 makeAst Event t2 = EventT <$> doParseType t2
 makeAst (Map v) t2 = MapT v <$> doParseType t2
-makeAst _ _ = Left "makeAst not yet implemented"
+makeAst _t1 _t2 = Left $ "makeAst not yet implemented for:" ++ show _t1 ++ show _t2
+
+instance Show ReflexAst where
+  show (MonadicT ast) = "m (" ++ show ast ++ ")"
+  show (DynamicT ast) = "Dynamic t (" ++ show ast ++ ")"
+  show (EventT ast) = "Event t (" ++ show ast ++ ")"
+  show (MapT k ast) = "Map (" ++ prettyPrint k ++ ") (" ++ show ast ++ ")"
+  show (TypeVar v) = prettyPrint v
